7aca721ca347e498c9220352d7b7707d5b2406ad
Chen-Chi Wang <Wangmasaki0318@gmail.com>
2023-02-10 21:31:59 +0000

    I make the list becomes manipulatable.

--- csvToPy.py
+++ csvToPy.py
@@ -1,7 +1,22 @@
-import csv 
+# use pandsa to manipulate the dataset
+import pandas as csvr
+from functools import cmp_to_key
+list =csvr.read_csv('footballDataset.csv')
 
-with open ('footballDataset.csv','r') as csvToPy: 
-    csv_reader = csv.reader(csvToPy) 
-    next(csvToPy)
-    for line in csv_reader:
-       print(line) 
+# create a list that store the data which has the special case of team
+Premier_league_teams = list[(list['League']=='Premier League')]
+
+season_09_to10=list[(list['Season']=='2009/2010')]
+season_10_to11=list[(list['Season']=='2010/2011')]
+## ie rank 5 team rank
+team_rank5=list[(list['Rank']==5)]
+team_rank2=list[(list['Rank']==2)]
+team_rank1=list[(list['Rank']==1)]
+
+# this codes tell how many teams is from specific season (doesn't have to be season it's modifiable) ie.(2009 to 2010 = 130 team)
+print(len(season_09_to10)) 
+print(len(season_10_to11)) 
+print(len(team_rank1))
+print(len(team_rank2))
+print(len(team_rank5))
+## create a function 
