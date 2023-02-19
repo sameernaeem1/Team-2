24f652fa5054d34cce7dae7097dfabfe91af4d35
Chen-Chi Wang <Wangmasaki0318@gmail.com>
2023-02-19 13:51:59 +0000

    found it, use iloc function to modify mutile rows

--- csvToPy.py
+++ csvToPy.py
@@ -1,7 +1,19 @@
-import csv 
+#import csv 
 
-with open ('Dateset.csv','r') as csvToPy: 
-    csv_reader = csv.reader(csvToPy) 
-    next(csvToPy)
-    for line in csv_reader:
-       print(line) 
+#with open ('Dateset.csv','r') as csvToPy: 
+ #   csv_reader = csv.reader(csvToPy) 
+  #  next(csvToPy)
+   # for line in csv_reader:
+    #   print(line) 
+
+
+import pandas as pd 
+df = pd.read_csv("Dateset.csv")
+#print(df)  
+
+#print(df.head(4)[['Team','Games','Season']])
+
+
+
+## Found it guys, use iloc function if you want to modify mutilple rows
+print(df.iloc[1])
