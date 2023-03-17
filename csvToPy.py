ccf85e6c953e9536cd885e0e03b078027a69b4e1
Chen-Chi Wang <wangmasaki0318@gmail.com>
2023-03-17 13:10:50 +0000

    USER INTERFACE

--- csvToPy.py
+++ csvToPy.py
@@ -1,8 +1,50 @@
 import pandas as pd 
 df = pd.read_csv("Dateset.csv")
-print(df)  
 
-#print(df.head(4)[['Team','Games','Season']])
+## UI 
+def Userinterface():
+ while True:
+    userinput= input("choose the option\n0:display the whole team in whole season\n1:display the status of each team in whole season\n2:display a team\n3:display team number")
+    if userinput == '0':
+        print(df)
+    elif userinput=='q':
+       break
+    elif userinput=='1':
+      for index, row in df.iterrows():  
+       print(index,row)
+    elif userinput=='2':
+        ui2=input('choose the team:')
+        print(df.iloc[int(ui2)])
+    elif userinput=='3':
+       for index, row in df.iterrows():  
+        print(index,row ['Team'])
+
+
+
+
+## workflow
+Userinterface()
+
+
+
+
+
+
+
+
+## components
+
+## sort the list in an specific order 
+df.sort_values((['Rank','Points']), ascending=False)
+## specify the conditon to find the specific one, rank 1 teams in the past decade
+rank1=df.loc[df['Rank']==1]
+print(rank1)
+## iterate through each row
+for index, row in df.iterrows():  ## it list all team at same time, we can use this function to ask which team are they interested in.
+ print(index,row ['Team'])
+
+## the rank of arsenal is 3 , which means that permit us to allocate the specific location (R,C)
+print(df.iloc[3,4])
 
 ## top 10 team
 Chelsea=df.iloc[0]
@@ -17,24 +59,8 @@ Birmingham=df.iloc[8]
 sunderland=df.iloc[9]
 westHam=df.iloc[10]
 
-## Found it guys, use iloc function if you want to modify mutilple rows
+## access unique element
+print(df.head(4)[['Team','Games','Season']])
+
+## use iloc function to modify the associated array
 df.iloc[3,1]
-
-## this means that the rank of arsenal is 3 , which means that permit us to allocate the specific location (R,C)
-print(df.iloc[3,4])
-## iterate through each row
-for index, row in df.iterrows():  ## it list all team at same time, we can use this function to ask which team are they interested in.
- print(index,row ['Team'])
-
-## specify the conditon to find the specific one, rank 1 teams in the past decade
-rank1=df.loc[df['Rank']==1]
-print(rank1)
-
-## sort the list in an specific order 
-df.sort_values((['Rank','Points']), ascending=False)
-
-## details of each team
-for index, row in df.iterrows():  
- print(index,row)
-
-## test merge thing
