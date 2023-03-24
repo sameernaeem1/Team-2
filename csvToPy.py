0f7f6ca2c3cabdb069b3a9718c22673701cccb07
Chen-Chi Wang <wangmasaki0318@gmail.com>
2023-03-24 13:15:25 +0000

    update today's meetings

--- csvToPy.py
+++ csvToPy.py
@@ -70,7 +70,27 @@ def Userinterface():
        chosen_team_df=df[df['Team'] == chosen_team]
        user_keyword=input("Enter a keyword to search for the component of the team:(eg Points, rank or wins)")
        chosen_columns=[col for col in chosen_team_df.columns if user_keyword.lower() in col.lower()]
-       print(chosen_team_df[chosen_columns])
+       pltdf=[]
+       pltdf=chosen_team_df[chosen_columns]
+       years=[9/10,10/11,11/12,12/13,13/14,14/15,15/16,16/17,17/18,18/19,19/20,20/21,21/22]
+
+       pltdf.to_csv(index=False)
+    
+      
+       print(pltdf)
+       
+
+    
+    
+      
+       
+      
+
+     
+
+       
+
+
        if  user_keyword =='wins':
         totalnum=chosen_team_df[chosen_columns].sum()
         print(f"{chosen_team}  get {totalnum} ")
@@ -79,7 +99,7 @@ def Userinterface():
        elif user_keyword=='points':
         totalpoints=chosen_team_df[chosen_columns].sum()
         print(f"{chosen_team} got total {totalpoints} points")
-
+   
 
     elif userinput=='7':
       for i, team in enumerate(team_names):
--- visualisation.py
+++ visualisation.py
@@ -1,2 +1,10 @@
 import csvToPy
+import matplotlib.pyplot as grh
 csvToPy.Userinterface()
+
+
+fig,ax=grh.subplot()
+
+ax.plot(csvToPy.pltdf)
+
+
