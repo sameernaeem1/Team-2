5a31d005357262a449c77f0a4e586415f065f9e4
Raja Shaheryar Iftikhar <cberifti@ljmu.ac.uk>
2023-03-31 14:16:04 +0100

    save

--- csvToPy.py
+++ csvToPy.py
@@ -13,43 +13,19 @@ def Userinterface():
  while True:
     print("-----Premier League Season's 2009 to 2022-----")
     print()
-    userinput= input("0:Display the whole team in whole season\n1:Choose Team\n5:Display the asending order rank of each teams in whole seasons")
-    if userinput == '0':
-        print(df)
-
-    elif userinput=='q':
-       ## quit
-       break
-    
-    elif userinput=='1':
-       ##display the status of teams in whole seasons### 
-      for index, row in df.iterrows():  
-       print(index,row)
+    userinput= input("1:Display Teams\n2:Compare Teams\n3:Choose Team\nQ:Quit\nChoose an Option:")
+    if userinput == '1':
+        for i, team in enumerate(team_names):
+         print(f"{i+1}. {team}")
 
     elif userinput=='2':
-##display the status of that particular team### 
-      print("Select a team:")
-      for i, team in enumerate(team_names):
-         print(f"{i+1}. {team}")
-      choosen_index = int(input("Enter the number of the index team you want to select: ")) - 1
-      User_team = team_names[choosen_index]
-      User_team_data = df[df['Team'] == User_team]
-      print(User_team_data)
-
-    elif userinput=='3':
-       ##only shows the unique team##
-       for i, team in enumerate(team_names):
-         print(f"{i+1}. {team}")
-
-    elif userinput=='4':
          for i, team in enumerate(team_names):
              print(f"{i+1}. {team}")
          ui4=int(input('Choose a team:'))-1
          df1=df.iloc[int(ui4)]
          print("The team you have picked is:")
          print(df1['Team'])
-      
-         
+   
          ui42=int(input('Choose another team:'))-1
          #print(df.iloc[int(ui42)])
          df2=df.iloc[int(ui42)]
@@ -58,22 +34,14 @@ def Userinterface():
          dff=df1.compare(df2)
          print(dff)
 
-
-
-    elif userinput=='5':
-      ## print the whole teams in ascending order of rank.
-      print(df.sort_values((['Rank','Points']), ascending=True))
-
-
-  ## Ask the user to input a keyword to display the value
-    elif userinput=='6':
+    elif userinput=='3':
        for i, team in enumerate(team_names):
          print(f"{i+1}. {team}")
        chosen_team_index = int(input("Enter the number of the team you want to select: ")) - 1
        chosen_team = team_names[chosen_team_index]
-       print(f"Team:{chosen_team}")
+       print(f"Team Picked:{chosen_team}")
        chosen_team_df=df[df['Team'] == chosen_team]
-       user_keyword=input("Enter a keyword to search for the component of the team:(eg Points, rank or wins)")
+       user_keyword=input("Enter a keyword to search for the component of the team:(eg Points, rank or wins)\n")
        chosen_columns=[col for col in chosen_team_df.columns if user_keyword.lower() in col.lower()]
        a=chosen_team_df[chosen_columns].values.tolist()
        c=chosen_team_df['Season'].values.tolist()
@@ -97,6 +65,42 @@ def Userinterface():
        elif user_keyword=='points':
         totalpoints=chosen_team_df[chosen_columns].sum()
         print(f"{chosen_team} got total {totalpoints} points")
+
+   
+    elif userinput=='q' or userinput=='Q':
+       ## quit
+       break
+    
+   # elif userinput=='00':
+       ##display the status of teams in whole seasons### 
+   #  for index, row in df.iterrows():  
+    #   print(index,row)
+
+    elif userinput=='20':
+##display the status of that particular team### 
+      print("Select a team:")
+      for i, team in enumerate(team_names):
+         print(f"{i+1}. {team}")
+      choosen_index = int(input("Enter the number of the index team you want to select: ")) - 1
+      User_team = team_names[choosen_index]
+      User_team_data = df[df['Team'] == User_team]
+      print(User_team_data)
+
+    elif userinput=='30':
+       ##only shows the unique team##
+       for i, team in enumerate(team_names):
+         print(f"{i+1}. {team}")
+
+
+
+
+    elif userinput=='5':
+      ## print the whole teams in ascending order of rank.
+      print(df.sort_values((['Rank','Points']), ascending=True))
+
+
+  ## Ask the user to input a keyword to display the value
+ 
        
        
 
