e50658cb1d8b310d4c0edc39607a9f33e9145e9c
Tom Palmer <thomasjy2002@gmail.com>
2023-04-18 19:25:51 +0100

    test

--- tom-Visualisation.py
+++ tom-Visualisation.py
@@ -1,5 +1,4 @@
-import matplotlib.pyplot as plt
-df = pd.read_csv("Dataset.csv")
+ import matplotlib.pyplot as plt
 
 goals = [55, 65, 75, 85, 95, 105, 115]
 amountscored = [73,58,93,66,100,84,71,80,106,95,102,83,99]
@@ -8,5 +7,5 @@ fig.suptitle("Figure title", fontsize=20)
 ax.set_title("Square Numbers", fontsize=14)
 ax.set_xlabel("Value", fontsize=12)
 ax.set_ylabel("Square of Value", fontsize=12)
-ax.plot(input_values, squares, 'mD:')
+ax.plot(goals, amountscored, 'mD:')
 plt.show()
